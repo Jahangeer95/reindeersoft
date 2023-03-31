581bae16ab70ab4e5703bc9236956205cda07af5
Jahangeer95a <jahangeer@emifusion.com>
2023-03-31 20:41:36 +0500

    modified font fomily...

--- src/components/layout/WebLayout.tsx
+++ src/components/layout/WebLayout.tsx
@@ -1,22 +1,10 @@
 import React, { Fragment } from "react";
-import Head from "next/head";
 import Footer from "../template/footer";
 import Header from "../template/header/Header";
 
 const WebLayout: React.FC<React.PropsWithChildren> = (props) => {
   return (
     <Fragment>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
-          rel="stylesheet"
-        ></link>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        ></link>
-      </Head>
-
       <Header />
       <main>{props.children}</main>
       <Footer />
