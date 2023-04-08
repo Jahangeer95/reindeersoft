f4b518e2ddf32c0bec2e376dbb8cb237d121b812
Jahangeer95a <jahangeer@emifusion.com>
2023-04-08 13:25:02 +0500

    robotics and autonomy modified..

--- src/components/content/robotics-and-autonomy/computer-vision/index.tsx
+++ src/components/content/robotics-and-autonomy/computer-vision/index.tsx
@@ -1,4 +1,5 @@
 import { Container } from "react-bootstrap";
+import Image from "next/image";
 import styles from "./computerVision.module.scss";
 import * as data from "./computerVisionData";
 
@@ -7,11 +8,16 @@ const ComputerVision = () => {
     <section className={styles.computerVisionMain} id="robotics-app-six">
       <Container fluid="lg">
         <div className="d-flex flex-column align-items-center">
-          <h3 className="text-center">{data.title}</h3>
           <article>
-            {data.description.map((state, idx) => (
-              <p key={idx}>{state}</p>
-            ))}
+            <span>
+              <Image src={data.content.image} alt={data.title} />
+            </span>
+            <span>
+              <h3 className="text-center">{data.title}</h3>
+              {data.content.description.map((state, idx) => (
+                <p key={idx}>{state}</p>
+              ))}
+            </span>
           </article>
         </div>
       </Container>
--- src/components/content/robotics-and-autonomy/simulation/index.tsx
+++ src/components/content/robotics-and-autonomy/simulation/index.tsx
@@ -1,4 +1,5 @@
 import { Container } from "react-bootstrap";
+import Image from "next/image";
 import styles from "./simulation.module.scss";
 import * as data from "./simulationData";
 
@@ -7,11 +8,16 @@ const Simulation = () => {
     <section className={styles.simulationMain} id="robotics-app-five">
       <Container fluid="lg">
         <div className="d-flex flex-column align-items-center">
-          <h3 className="text-center">{data.title}</h3>
           <article>
-            {data.description.map((state, idx) => (
-              <p key={idx}>{state}</p>
-            ))}
+            <span>
+              <h3 className="text-center">{data.title}</h3>
+              {data.content.description.map((state, idx) => (
+                <p key={idx}>{state}</p>
+              ))}
+            </span>
+            <span>
+              <Image src={data.content.image} alt={data.title} />
+            </span>
           </article>
         </div>
       </Container>
