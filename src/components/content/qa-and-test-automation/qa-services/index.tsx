2f07ba5673dbcd766ef945d2d9fb7823df8f1acc
Jahangeer95a <jahangeer@emifusion.com>
2023-06-02 08:07:52 +0500

    bug modified...

--- src/components/content/qa-and-test-automation/qa-services/index.tsx
+++ src/components/content/qa-and-test-automation/qa-services/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import * as data from "./qaServicesData";
 import Services from "@/components/custom/services";
+import styles from "./serviceStyles.module.scss";
 
 const QaServices = () => {
-  return <Services title={data.title} servicesArray={data.qaServicesArray} />;
+  return (
+    <Services
+      title={data.title}
+      servicesArray={data.qaServicesArray}
+      serviceClass={styles.serviceMain}
+    />
+  );
 };
 
 export default QaServices;
