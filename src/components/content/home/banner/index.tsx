8a330972ca0d8e2bcdf75bc47ce883f6aaef734b
Jahangeer95a <jahangeer@emifusion.com>
2023-07-04 21:11:48 +0500

    image tag added in home banner...

--- src/components/content/home/banner/index.tsx
+++ src/components/content/home/banner/index.tsx
@@ -1,15 +1,23 @@
+import Image from "next/image";
 import CustomBanner from "@/components/custom/banner";
 import styles from "./banner.module.scss";
 
 const Banner = () => {
   return (
-    <CustomBanner
-      title="Reindeersoft - Empowering Innovation, Transforming Technology"
-      description="Innovating the digital space for businesses to grow.
+    <>
+      <CustomBanner
+        title="Reindeersoft - Empowering Innovation, Transforming Technology"
+        description="Innovating the digital space for businesses to grow.
       We build simple software solutions for complex problems."
-      list={[]}
-      imageClass={styles.homeBannerImage}
-    />
+        list={[]}
+        imageClass={styles.homeBannerImage}
+      />
+      <Image
+        src={require("../../../../assets/images/home/Liqq.gif")}
+        alt="banner-image"
+        style={{ display: "none" }}
+      />
+    </>
   );
 };
 
