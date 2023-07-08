23cee38017a1260d4004912f73d41d3720ad3ff1
Jahangeer95a <jahangeer@emifusion.com>
2023-07-08 20:40:55 +0500

    about us route modified...

--- src/components/content/about-us/banner/bannerData.ts
+++ src/components/content/about-us/banner/bannerData.ts
@@ -2,4 +2,4 @@ import { BannertitleType, BannerDescriptionType } from "@/types/contactUstype";
 
 export const title: BannertitleType = "About us";
 export const description: BannerDescriptionType =
-  "At ReindeerSoft, we are driven by a strong set of core values that shape our operations and define who we are. Ethics lies at the heart of everything we do, guiding our decisions and ensuring that we uphold the highest standards of integrity and accountability. We strive for excellence in all aspects of our work, constantly pushing the boundaries of innovation to deliver products and services that exceed expectations. We believe in mutual growth, fostering collaborative relationships with our clients, employees, and partners. By working together, we aim to create an environment that promotes growth, learning, and success for everyone involved. Additionally, we understand the importance of cost effectiveness and are committed to providing efficient solutions that deliver value for our clients while optimizing their investment.";
+  "Our highly skilled teams are poised to kickstart your next project.";
--- src/components/content/about-us/banner/index.tsx
+++ src/components/content/about-us/banner/index.tsx
@@ -1,18 +1,24 @@
 import React from "react";
+import Link from "next/link";
 import { Container } from "react-bootstrap";
+import { TiInfoLargeOutline } from "react-icons/ti";
 import * as data from "./bannerData";
 import styles from "./Banner.module.scss";
 
 const Banner = () => {
   return (
-    <section className={styles.IotMain}>
+    <section className={styles.aboutBannerMain}>
       <Container>
         <div>
           <article>
+            <span>
+              <TiInfoLargeOutline />
+            </span>
             <h1>{data.title}</h1>
             <div>
               <p>{data.description}</p>
             </div>
+            <Link href="/contact-us">Contact Us</Link>
           </article>
         </div>
       </Container>
