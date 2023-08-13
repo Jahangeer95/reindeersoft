28b77922e0bd02ed779eb6040a5179676b13b830
Jahangeer95a <jahangeer@emifusion.com>
2023-08-13 08:40:01 +0500

    how we work styles modified...

--- src/components/routes/home/how-we-work/index.tsx
+++ src/components/routes/home/how-we-work/index.tsx
@@ -24,12 +24,6 @@ const HowWeWork = () => {
     }
   }, [router]);
 
-  useEffect(() => {
-    if (window.innerWidth < 768) {
-      setMobileView(true);
-    }
-  }, []);
-
   return (
     <section
       className={styles.howWeWorkMain}
@@ -45,57 +39,55 @@ const HowWeWork = () => {
         <div className="d-flex flex-column align-items-center">
           <h2>How We Work</h2>
           <article>
-            {mobileView ? (
-              <div>
-                <MobileViewTable
-                  title={Object.values(data.engagementModalData[1])[0]}
-                  description={[
-                    ...Object.values(data.engagementModalData[1]).slice(1),
-                  ]}
-                />
-                <MobileViewTable
-                  title={Object.values(data.engagementModalData[2])[0]}
-                  description={[
-                    ...Object.values(data.engagementModalData[2]).slice(1),
-                  ]}
-                />
-                <MobileViewTable
-                  title={Object.values(data.engagementModalData[3])[0]}
-                  description={[
-                    ...Object.values(data.engagementModalData[3]).slice(1),
-                  ]}
-                />
-              </div>
-            ) : (
-              <table>
-                <thead>
-                  {data.engagementModalData.slice(0, 1).map((state, idx) => {
-                    return (
-                      <tr key={idx}>
-                        <th>{state.column1}</th>
-                        <th>{state.column2}</th>
-                        <th>{state.column3}</th>
-                        <th>{state.column4}</th>
-                        <th>{state.column5}</th>
-                      </tr>
-                    );
-                  })}
-                </thead>
-                <tbody>
-                  {data.engagementModalData.slice(1).map((state, idx) => {
-                    return (
-                      <tr key={idx}>
-                        <td>{state.column1}</td>
-                        <td>{state.column2}</td>
-                        <td>{state.column3}</td>
-                        <td>{state.column4}</td>
-                        <td>{state.column5}</td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            )}
+            <div className={styles.mobileView}>
+              <MobileViewTable
+                title={Object.values(data.engagementModalData[1])[0]}
+                description={[
+                  ...Object.values(data.engagementModalData[1]).slice(1),
+                ]}
+              />
+              <MobileViewTable
+                title={Object.values(data.engagementModalData[2])[0]}
+                description={[
+                  ...Object.values(data.engagementModalData[2]).slice(1),
+                ]}
+              />
+              <MobileViewTable
+                title={Object.values(data.engagementModalData[3])[0]}
+                description={[
+                  ...Object.values(data.engagementModalData[3]).slice(1),
+                ]}
+              />
+            </div>
+
+            <table>
+              <thead>
+                {data.engagementModalData.slice(0, 1).map((state, idx) => {
+                  return (
+                    <tr key={idx}>
+                      <th>{state.column1}</th>
+                      <th>{state.column2}</th>
+                      <th>{state.column3}</th>
+                      <th>{state.column4}</th>
+                      <th>{state.column5}</th>
+                    </tr>
+                  );
+                })}
+              </thead>
+              <tbody>
+                {data.engagementModalData.slice(1).map((state, idx) => {
+                  return (
+                    <tr key={idx}>
+                      <td>{state.column1}</td>
+                      <td>{state.column2}</td>
+                      <td>{state.column3}</td>
+                      <td>{state.column4}</td>
+                      <td>{state.column5}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
           </article>
         </div>
       </Container>
