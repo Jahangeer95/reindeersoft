b83776891af997075fad3efb2e285cf9e9031fea
Jahangeer95a <jahangeer@emifusion.com>
2023-08-14 06:55:18 +0500

    faq's modified with provided data...

--- src/components/routes/home/faq/faqData.ts
+++ src/components/routes/home/faq/faqData.ts
@@ -4,18 +4,30 @@ export const title: FaqsTitleType = "Frequently Asked Questions";
 
 export const faqsContent: FaqsContentType = [
   {
-    question: "Do you offer custom software development services?",
+    question: "Why should we choose your software company for our project?",
     answer:
-      " We’re open to working with any industry. as our team’s strategy, project management, and development skills proved to be effective in SaaS, MedTech, or even government projects. Focusteck has even provided support to other software development companies.",
+      "We have an exceptional team comprising BS, MS, and PhD graduates from world-class universities, backed by over a decade of experience across various software domains. With a proven track record spanning various industries and a deep understanding of diverse software technologies,  we bring unmatched expertise to every project. Our commitment to ethics, excellence, and cost-effectiveness guarantees your project's success within your specified timeline and budget.",
   },
   {
-    question: "Do you offer custom software development services?",
+    question:
+      "What business model would you recommend among Project Engagement, Staff Augmentation, and Dedicated Development Team?",
     answer:
-      " We’re open to working with any industry. as our team’s strategy, project management, and development skills proved to be effective in SaaS, MedTech, or even government projects. Focusteck has even provided support to other software development companies.",
+      "The recommended business model depends on your project's scale, complexity, timeline, budget, and specific requirements. Project Engagement is ideal for well-defined projects with a clear scope. Staff Augmentation suits businesses needing to expand their team temporarily. Dedicated Development Team is perfect for long-term collaborations requiring a skilled and dedicated extension of your in-house team. Our experts will guide you to select the most suitable model for your unique needs while ensuring cost-effectiveness.",
   },
   {
-    question: "Do you offer custom software development services?",
+    question:
+      "What practices does your team employ to ensure quality assurance throughout the project?",
     answer:
-      " We’re open to working with any industry. as our team’s strategy, project management, and development skills proved to be effective in SaaS, MedTech, or even government projects. Focusteck has even provided support to other software development companies.",
+      "Our team possesses an in-depth understanding of various quality assurance software, with refined skills in defining effective QA processes, deploying advanced UI and CLI test automation frameworks, seamless integration of tests into CI/CD pipelines, and precise automation of manual procedures.  Adhering to industry best practices, we provide an extensive range of manual and automated testing services. Our quality assurance process encompasses Requirement Analysis, Test Planning & Strategy, Test Plan Review & Approval, and Test Execution & Bug Reporting to ensure the delivery of high-quality products.",
+  },
+  {
+    question: "How do you handle changes in project scope or requirements?",
+    answer:
+      "We recognize that project scope and requirements can evolve. Our agile approach allows us to accommodate changes effectively. We collaborate closely with you to assess the impact, provide transparent communication, and adjust the project plan as needed to ensure your goals are met successfully.",
+  },
+  {
+    question: "Do you offer ongoing support and maintenance?",
+    answer:
+      "Yes, we provide comprehensive ongoing support and maintenance services. Our dedicated team ensures that your software remains up-to-date, secure, and fully functional even after deployment. We offer different levels of support tailored to your specific needs, guaranteeing a seamless experience and the sustained performance of your solution.",
   },
 ];
