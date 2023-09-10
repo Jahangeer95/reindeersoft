7973bff7717d809f0d2fabb4c0d9690e27bacb20
Jahangeer95a <jahangeer@emifusion.com>
2023-09-10 07:29:50 +0500

    footer modified...

--- src/components/routes/large-language-model/service-overview/serviceOverviewData.ts
+++ src/components/routes/large-language-model/service-overview/serviceOverviewData.ts
@@ -6,6 +6,6 @@ import {
 export const ServiceOverViewTitle: ServiceOverviewTitleType =
   "Service Overview";
 export const ServiceOverviewDescription: ServiceOverviewDescriptionType = [
-  "We offers a comprehensive suite of solutions centred around Large Language Models (LLMs), with a focus on versatility, scalability, and efficiency. We provide the tools and expertise to seamlessly incorporate LLMs into your existing systems or build new applications from the ground up.",
+  "We offer a comprehensive suite of solutions centred around Large Language Models (LLMs), with a focus on versatility, scalability, and efficiency. We provide the tools and expertise to seamlessly incorporate LLMs into your existing systems or build new applications from the ground up.",
   "Our team of experts is dedicated to helping you leverage the power of LLMs to revolutionize your business processes and enhance customer experiences.. We provide personalized services tailored to your business needs, propelling you into the forefront of the AI-powered future. Our team will work closely with you to understand your unique requirements and design customised solutions that align with your business goals.",
 ];
--- src/components/template/footer/index.tsx
+++ src/components/template/footer/index.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
       <Container fluid="lg">
         <div>
           <article>
-            <Company />
+            {/* <Company /> */}
 
             <Contact />
           </article>
