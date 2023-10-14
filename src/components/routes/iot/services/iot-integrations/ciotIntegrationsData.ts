185a0e14259d64589145c48da090f8b7991ccbcd
Jahangeer95a <jahangeer@emifusion.com>
2023-10-14 12:48:43 +0500

    new image added..

--- src/components/routes/iot/services/iot-integrations/ciotIntegrationsData.ts
+++ src/components/routes/iot/services/iot-integrations/ciotIntegrationsData.ts
@@ -1,4 +1,4 @@
-import iotDevelopment from "@/assets/images/iot/iot.jpg";
+import iotIntegration from "@/assets/images/iot/iot-integration.jpg";
 import { IoTServiceContent, IoTServiceTitleType } from "@/types/iot.type";
 
 export const IoTIntegrationsTitle: IoTServiceTitleType = "IoT Integrations";
@@ -6,5 +6,5 @@ export const IoTIntegrationsContent: IoTServiceContent = {
   description: [
     "Integrating IoT devices and platforms with existing systems and applications can be a complex process, but our team has the technical expertise to make it seamless. We work with you to ensure that all systems are fully integrated and that data flows smoothly between them. This allows you to leverage the power of IoT without disrupting your existing workflows or processes.",
   ],
-  image: iotDevelopment,
+  image: iotIntegration,
 };
--- src/components/routes/iot/services/iot-security/iotSecurityData.ts
+++ src/components/routes/iot/services/iot-security/iotSecurityData.ts
@@ -1,4 +1,4 @@
-import iotDevelopment from "@/assets/images/iot/iot.jpg";
+import iotSecurity from "@/assets/images/iot/iot-security.jpg";
 import { IoTServiceContent, IoTServiceTitleType } from "@/types/iot.type";
 
 export const IoTSecurityTitle: IoTServiceTitleType = "IoT Security";
@@ -6,5 +6,5 @@ export const IoTSecurityContent: IoTServiceContent = {
   description: [
     "Security is a top priority when it comes to IoT, and our team has extensive experience in IoT security. We take a multi-layered approach to security, including encryption, authentication, and access control. We design our solutions with security in mind from the ground up, ensuring that your data is safe and secure at all times.",
   ],
-  image: iotDevelopment,
+  image: iotSecurity,
 };
