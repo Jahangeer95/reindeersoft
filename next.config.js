3e9eece7a07a930bec60fb37b7146ad142768df0
Jahangeer95a <jahangeer@emifusion.com>
2023-10-21 20:47:42 +0500

    few chabges done...

--- next.config.js
+++ next.config.js
@@ -4,3 +4,13 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "export",
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+// module.exports = nextConfig;
