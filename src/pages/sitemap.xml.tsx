76573a2a48792027db18f6d44a6654e45406401a
Jahangeer95a <jahangeer@emifusion.com>
2023-11-26 20:49:56 +0500

    sitemap page modified to make it work properly....

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -1,15 +1,10 @@
-import React from "react";
-
-export default function generateSitemap() {
-  return `
-    <?xml version="1.0" encoding="UTF-8"?>
+function generateSitemap() {
+  return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset
       xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
       xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
             http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-<!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
-
 
 <url>
   <loc>https://reindeersoft.com/</loc>
@@ -61,7 +56,29 @@ export default function generateSitemap() {
   <lastmod>2023-11-26T06:39:07+00:00</lastmod>
   <priority>0.80</priority>
 </url>
-
-
 </urlset>`;
 }
+
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export async function getServerSideProps({ res }: any) {
+  // We make an API call to gather the URLs for our site
+  // const request = await fetch(EXTERNAL_DATA_URL);
+  // const posts = await request.json();
+
+  // We generate the XML sitemap with the posts data
+  const sitemap = generateSitemap();
+
+  res.setHeader("Content-Type", "text/xml");
+  // we send the XML to the browser
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
