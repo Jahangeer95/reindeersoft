ba3b320f81221a5c1b9e5e51e691965b8865a22b
Jahangeer95a <jahangeer@emifusion.com>
2023-11-26 22:04:10 +0500

    form functionality added...

--- src/components/custom/contactUsForm/index.tsx
+++ src/components/custom/contactUsForm/index.tsx
@@ -1,11 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useRef } from "react";
+import * as Yup from "yup";
 import { Form } from "react-bootstrap";
-import styles from "./formStyle.module.scss";
 import countryList from "country-list";
 import { useFormik } from "formik";
-import * as Yup from "yup";
+import emailjs from "@emailjs/browser";
+import { ToastContainer, toast } from "react-toastify";
+import styles from "./formStyle.module.scss";
+import "react-toastify/dist/ReactToastify.css";
 
 const ContactUsForm = () => {
+  const formRef = useRef<any>();
   const formik = useFormik({
     initialValues: {
       "first-name": "",
@@ -15,8 +19,8 @@ const ContactUsForm = () => {
       seekingFor: "",
       message: "",
     },
-    onSubmit: (values) => {
-      console.log(values);
+    onSubmit: (values, { resetForm }) => {
+      submitHandler(resetForm);
     },
     validationSchema: Yup.object({
       "first-name": Yup.string().required("First Name  is required!"),
@@ -34,98 +38,128 @@ const ContactUsForm = () => {
   // should use currying concept for change Handler
 
   const whatAreYouSeekingArr: string[] = [
-    "Item 1",
-    "Item 2",
-    "Item 3",
-    "Item 4",
-    "Item 5",
+    "Web and Mobile Applications",
+    "Dedicated Development Team",
+    "Robotics and Autonomy",
+    "Quality Assurance and Test Automation",
+    "Cloud Application Development",
+    "Internet Of Things",
+    "Large Language Model",
   ];
 
-  useEffect(() => {
-    console.log(formik);
-  });
+  function submitHandler(resetForm: any) {
+    emailjs
+      .sendForm(
+        "service_smboof4",
+        "template_390m1gl",
+        formRef.current,
+        "j_T8kahx4XkQ4_SzY"
+      )
+      .then(() => {
+        toast.success("Message has been sent successfully...", {
+          position: toast.POSITION.TOP_RIGHT,
+        });
+      })
+      .catch((error) => {
+        toast.error(
+          error.message || "Failed to send message, Please try again...",
+          { position: toast.POSITION.TOP_RIGHT }
+        );
+      })
+      .finally(() => resetForm());
+  }
 
   return (
-    <Form className={styles.formMain} onSubmit={formik.handleSubmit}>
-      <Form.Control
-        type="text"
-        className={
-          formik.errors["first-name"] && formik.touched["first-name"]
-            ? styles.error
-            : ""
-        }
-        placeholder="First Name"
-        {...formik.getFieldProps("first-name")}
-      />
-      <Form.Control
-        type="text"
-        className={
-          formik.errors["last-name"] && formik.touched["last-name"]
-            ? styles.error
-            : ""
-        }
-        placeholder="Last Name"
-        {...formik.getFieldProps("last-name")}
-      />
-      <Form.Control
-        type="string"
-        className={
-          formik.errors["email"] && formik.touched["email"] ? styles.error : ""
-        }
-        placeholder="Email"
-        {...formik.getFieldProps("email")}
-      />
-      <Form.Select
-        {...formik.getFieldProps("country")}
-        className={
-          formik.errors["country"] && formik.touched["country"]
-            ? styles.error
-            : formik.touched["country"]
-            ? styles.selectText
-            : ""
-        }
+    <>
+      <ToastContainer />
+
+      <Form
+        className={styles.formMain}
+        onSubmit={formik.handleSubmit}
+        ref={formRef}
       >
-        <option value="none" selected hidden>
-          Select Country (optional)
-        </option>
-        {countryList.getNames().map((state: string, idx: number) => (
-          <option key={idx} value={state}>
-            {state}
+        <Form.Control
+          type="text"
+          className={
+            formik.errors["first-name"] && formik.touched["first-name"]
+              ? styles.error
+              : ""
+          }
+          placeholder="First Name"
+          {...formik.getFieldProps("first-name")}
+        />
+        <Form.Control
+          type="text"
+          className={
+            formik.errors["last-name"] && formik.touched["last-name"]
+              ? styles.error
+              : ""
+          }
+          placeholder="Last Name"
+          {...formik.getFieldProps("last-name")}
+        />
+        <Form.Control
+          type="string"
+          className={
+            formik.errors["email"] && formik.touched["email"]
+              ? styles.error
+              : ""
+          }
+          placeholder="Email"
+          {...formik.getFieldProps("email")}
+        />
+        <Form.Select
+          {...formik.getFieldProps("country")}
+          className={
+            formik.errors["country"] && formik.touched["country"]
+              ? styles.error
+              : formik.touched["country"]
+              ? styles.selectText
+              : ""
+          }
+        >
+          <option value="none" selected hidden>
+            Select Country (optional)
           </option>
-        ))}
-      </Form.Select>
-      <Form.Select
-        {...formik.getFieldProps("seekingFor")}
-        className={
-          formik.errors["seekingFor"] && formik.touched["seekingFor"]
-            ? styles.error
-            : formik.touched["seekingFor"]
-            ? styles.selectText
-            : ""
-        }
-      >
-        <option value="none" selected hidden>
-          What are you seeking for?
-        </option>
-        {whatAreYouSeekingArr.map((state: string, idx: number) => (
-          <option key={idx} value={state}>
-            {state}
+          {countryList.getNames().map((state: string, idx: number) => (
+            <option key={idx} value={state}>
+              {state}
+            </option>
+          ))}
+        </Form.Select>
+        <Form.Select
+          {...formik.getFieldProps("seekingFor")}
+          className={
+            formik.errors["seekingFor"] && formik.touched["seekingFor"]
+              ? styles.error
+              : formik.touched["seekingFor"]
+              ? styles.selectText
+              : ""
+          }
+        >
+          <option value="none" selected hidden>
+            What are you seeking for?
           </option>
-        ))}
-      </Form.Select>
-      <Form.Control
-        as="textarea"
-        className={
-          formik.errors["message"] && formik.touched["message"]
-            ? styles.error
-            : ""
-        }
-        rows={4}
-        placeholder="Message"
-        {...formik.getFieldProps("message")}
-      />
-      <Form.Control type="submit" value="Submit" />
-    </Form>
+          {whatAreYouSeekingArr.map((state: string, idx: number) => (
+            <option key={idx} value={state}>
+              {state}
+            </option>
+          ))}
+        </Form.Select>
+        <Form.Control
+          as="textarea"
+          className={
+            formik.errors["message"] && formik.touched["message"]
+              ? styles.error
+              : ""
+          }
+          rows={4}
+          placeholder="Message"
+          {...formik.getFieldProps("message")}
+        />
+        <Form.Control type="submit" value="Submit" />
+      </Form>
+    </>
   );
 };
 
