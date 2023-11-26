2f9e674109f1371727d328a9e187938f29992c30
Jahangeer95a <jahangeer@emifusion.com>
2023-11-26 15:20:48 +0500

    sitemap added in pages directory...

---
+++ src/pages/sitemap.xml.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+export default function generateSitemap() {
+  return `
+    <?xml version="1.0" encoding="UTF-8"?>
+<urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+<!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
+
+
+<url>
+  <loc>https://reindeersoft.com/</loc>
+  <lastmod>2023-11-26T06:39:07+00:00</lastmod>
+  <priority>1.00</priority>
+</url>
+<url>
+  <loc>https://reindeersoft.com/about-us</loc>
+  <lastmod>2023-11-26T06:39:07+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://reindeersoft.com/contact-us</loc>
+  <lastmod>2023-11-26T06:39:07+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://reindeersoft.com/web-and-mobile-applications</loc>
+  <lastmod>2023-11-26T06:39:07+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://reindeersoft.com/dedicated-development-team</loc>
+  <lastmod>2023-11-26T06:39:07+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://reindeersoft.com/robotics-and-autonomy</loc>
+  <lastmod>2023-11-26T06:39:07+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://reindeersoft.com/qa-and-test-automation</loc>
+  <lastmod>2023-11-26T06:39:07+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://reindeersoft.com/cloud-application-development</loc>
+  <lastmod>2023-11-26T06:39:07+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://reindeersoft.com/internet-of-things</loc>
+  <lastmod>2023-11-26T06:39:07+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://reindeersoft.com/large-language-model</loc>
+  <lastmod>2023-11-26T06:39:07+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+
+
+</urlset>`;
+}
