b707b6b56bd796ce489ca6858927b9754a05aab1
Jahangeer95a <jahangeer@emifusion.com>
2023-12-03 12:44:54 +0500

    logo updated...

--- src/utils/meta/index.tsx
+++ src/utils/meta/index.tsx
@@ -10,7 +10,13 @@ const MetaInformation = (props: MetaInformationPropTypes) => {
       <meta name="description" content={description} />
       <link rel="canonical" href={chononicalUrl} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/logo.jpg" />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      {/* <link rel="icon" href="/logo.ico" /> */}
       {/* <link rel="icon" href="/favicon.ico" /> */}
       {/* <link rel="preload" href="../assets/images/home/Liqq.gif" as="image" /> */}
     </Head>
